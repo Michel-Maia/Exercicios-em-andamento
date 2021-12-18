770fc8431cdac73594dab7e32bfb5318faa2f490
Michel Maia <programador.michel.maia@gmail.com>
2021-12-18 01:13:08 -0300

    commit
    condicoes

---
+++ condicoes/main.js
@@ -0,0 +1,10 @@
+var vel = 60.5
+console.log ('A velocidade do seu carro é $(vel) KM/h')
+
+if (vel > 60) {
+    console.log ('Você ultrapassou a velocidade permitida. FOI MULTADO!')
+}
+
+console.log('Sempre use o sinto de segurança!')
+
+
---
+++ condicoes/main02.js
@@ -0,0 +1,4 @@
+var idade = 22
+if (idade < 18) {
+    console.log('Não vota')
+}
