72ae51dfa7c700dc3bc1da870ce55255f5707319
Michel Maia <programador.michel.maia@gmail.com>
2022-01-13 14:13:48 -0300

    commit aulas

---
+++ ReactJS/aula03/thinking-react/src/mock.js
@@ -0,0 +1,24 @@
+const mock = [
+  {
+    id: 1,
+    name: 'Michel',
+    skills: ['React', 'Node', 'CSS', 'Webpack']
+  },
+  {
+    id: 2,
+    name: 'Lilian',
+    skills: ['HTML', 'React Native', 'Go', 'JS']
+  },
+  {
+    id: 3,
+    name: 'Angelica',
+    skills: ['Assembly']
+  },
+  {
+    id: 4,
+    name: 'Enzo',
+    skills: ['Reason']
+  }
+]
+
+export default mock
