d6a2a1dd12ee657f8105c5017ebb77db18f82678
Michel Maia <programador.michel.maia@gmail.com>
2022-02-16 18:08:54 -0300

    commit
    menu

--- .NET c#/Calculator/Program.cs
+++ .NET c#/Calculator/Program.cs
@@ -19,10 +19,22 @@ namespace Calculator
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Divisão");
             Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
 
             Console.WriteLine("------------");
             Console.WriteLine("Selecione uma opção: ");
+
             short res = short.Parse(Console.ReadLine());
+
+            switch(res) {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+
+            }
 
         }
 
@@ -41,6 +53,7 @@ namespace Calculator
             float resultado = v1 + v2;
             Console.WriteLine("O resultado da soma é " + resultado);
             Console.ReadKey();
+            Menu();
 
         }
 
@@ -58,6 +71,7 @@ namespace Calculator
             float resultado = v1 - v2;
             Console.WriteLine($"O resultado da subtração é {resultado}");
             Console.ReadKey();
+            Menu();
 
         }
 
@@ -75,6 +89,7 @@ namespace Calculator
             float resultado = v1 / v2;
             Console.WriteLine($"O resultado da divisão é {resultado}");
             Console.ReadKey();
+            Menu();
 
         }
 
@@ -92,6 +107,7 @@ namespace Calculator
             float resultado = v1 * v2;
             Console.WriteLine("O resultado da multiplicação é " + (v1 * v2));
             Console.ReadKey();
+            Menu();
 
         }
 
