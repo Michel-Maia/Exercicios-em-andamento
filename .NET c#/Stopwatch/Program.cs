daa4dbb3c16c26c257618dc37763aa55a60263da
Michel Maia <programador.michel.maia@gmail.com>
2022-03-03 13:02:57 -0300

    finalizando

--- .NET c#/Stopwatch/Program.cs
+++ .NET c#/Stopwatch/Program.cs
@@ -21,11 +21,31 @@ namespace Stopwatch
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length-1,1));
             int time = int.Parse(data.Substring(0,data.Length-1));
+            int multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
             
-            Console.WriteLine(type);
-            Console.WriteLine(time);
 
         }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        } 
 
         static void Start (int time)
         {            
@@ -43,6 +63,7 @@ namespace Stopwatch
             Console.Clear();
             Console.WriteLine("Stopwatch finalizado");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
