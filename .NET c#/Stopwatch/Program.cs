06fe84633f969c3f19a1f21d18f094cd4fc80058
Michel Maia <programador.michel.maia@gmail.com>
2022-03-03 12:45:07 -0300

    testando
    verificando o uso do char.Parse e int.Parse

--- .NET c#/Stopwatch/Program.cs
+++ .NET c#/Stopwatch/Program.cs
@@ -19,6 +19,11 @@ namespace Stopwatch
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length-1,1));
+            int time = int.Parse(data.Substring(0,data.Length-1));
+            
+            Console.WriteLine(type);
+            Console.WriteLine(time);
 
         }
 
