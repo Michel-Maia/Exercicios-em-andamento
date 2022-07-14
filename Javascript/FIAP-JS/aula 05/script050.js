716d79faaae65eca9e37d42108ec9816748f1abc
Michel Maia <programador.michel.maia@gmail.com>
2022-07-14 14:39:15 -0300

    commit
    aula de operadores matemáticos

---
+++ Javascript/FIAP-JS/aula 05/script050.js
@@ -0,0 +1,17 @@
+// Exemplo operação matemática 
+
+console.log("Adição 4+4: ");
+console.log(4+4);
+
+console.log("Subtração 4-2: ");
+console.log(4-2);
+
+console.log("Multiplicação 4x2: ");
+console.log(4*2);
+
+console.log("Divisão 4/2: ");
+console.log(4/2);
+
+console.log("Resto da divisão de 10 por 3: ");
+console.log(10%3);
+
---
+++ Javascript/FIAP-JS/aula 05/script051.js
@@ -0,0 +1,13 @@
+// Exemplo funções matemáticas 
+
+var num = 5;
+console.log("Valor de num: "+num);
+
+num = Math.pow(5,2);
+console.log("Valor de num: "+num);
+
+num = num - Math.random();
+console.log("Valor de num: "+num);
+
+num = num * Math.PI;
+console.log("Valor de num: " +num);
