9f47b23ced9b121e31659575880aa2e2ed6301d2
Michel Maia <programador.michel.maia@gmail.com>
2022-08-02 15:50:48 -0300

    commit
    aula js hcode

---
+++ Javascript/HCode-JS/1parte/002script.js
@@ -0,0 +1,24 @@
+let cor = "azul"
+
+if (cor === "verde") {
+    console.log("siga");
+
+} else if (cor === "amarelo") {
+    console.log("atenção");
+
+} else if (cor === "vermelho") {
+    console.log("pare");
+}
+
+
+/* 
+
+= atribuição 
+== comparação de valor
+=== compara valor e tipo
+!= compara valor
+!== compara de o valor e tipo são diferentes
+
+&& duas condições são verdadeiras 
+
+*/
---
+++ Javascript/HCode-JS/1parte/004script.js
@@ -0,0 +1,5 @@
+let n = 5;
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${i} X ${n} = ${i*n}`);
+}
