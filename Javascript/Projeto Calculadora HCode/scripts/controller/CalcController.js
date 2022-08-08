3994450c9cd102b9f76df0a084ab970b4e0143c3
Michel Maia <programador.michel.maia@gmail.com>
2022-08-08 17:26:41 -0300

    commit

--- Javascript/Projeto Calculadora HCode/scripts/controller/CalcController.js
+++ Javascript/Projeto Calculadora HCode/scripts/controller/CalcController.js
@@ -1,4 +1,35 @@
 class CalcController {
 
-    
+    constructor() {
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+        let displayCalcE1 = document.querySelector("#display");
+        let dateE1 = document.querySelector("#data");
+        let timeE1 = document.querySelector("#hora");
+
+        displayCalcE1.innerHTML = "7"; 
+        dateE1.innerHTML = "05/05/2022"; 
+        timeE1.innerHTML = "17:23"; 
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(valor){
+        return this._currentDate = valor;
+    }
 }
+
---
+++ Javascript/Projeto Calculadora HCode/scripts/controller/rascunho.md
@@ -0,0 +1,25 @@
+# Anotações
+
+
+Atributo - similar a uma variável, porém com mais recursos 
+
+o comando interno this , referencia atributos e métodos
+
+o processo de encapsulamento, controla o acesso a um atributo ou método (quem pode acessar/ qual regra)
+
+Os tipos mais comuns de encapsulamento são
+- public (todos acessam)
+- protected (só fala com atributos e métodos da mesma classe e classe Pai)
+- private (somente atributos e métodos da mesma classe) this._
+
+
+Atribuir ou Recuperar
+- Valores dos atributos
+
+Os métodos 
+- getters
+- setters 
+permitem definir como acessar os valores
+
+# DOM
+Cada elemento HTML vira Objeto da coleção HTMLDocument 
