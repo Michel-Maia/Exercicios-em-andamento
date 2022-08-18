fe5cc991fa8140e4fb38c72eaac1901e42a820ba
Michel Maia <programador.michel.maia@gmail.com>
2022-08-18 19:03:21 -0300

    commit

---
+++ Javascript/Rocketseat/tipos_de_dados/number.js
@@ -0,0 +1,12 @@
+
+/*
+Number - números
+
+  52 - inteiro 
+  2.30 - reais - float
+  NaN - Not a Number
+  Infinity - infinito 
+*/ 
+
+
+console.log(33)
---
+++ Javascript/Rocketseat/tipos_de_dados/string.js
@@ -0,0 +1,8 @@
+// String - cadeia de caracteres abcdef...
+//   "" aspas duplas
+//    '' aspas simples
+//    `` template strings 
+
+
+
+console.log('Testando JS')
