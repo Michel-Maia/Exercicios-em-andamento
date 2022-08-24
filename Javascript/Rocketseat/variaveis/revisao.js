2dc440bacc97ade3c99c70ff2a4005cdfb71e643
Michel Maia <programador.michel.maia@gmail.com>
2022-08-24 17:47:44 -0300

    commit
    aula js

--- Javascript/Rocketseat/variaveis/revisao.js
+++ Javascript/Rocketseat/variaveis/revisao.js
@@ -22,15 +22,15 @@ let isHuman = true
 // escrita de texto + variáveis
 
 // concatenando valores
-console.log('o' + name + 'tem' + age + 'anos')
+//console.log('o' + name + 'tem' + age + 'anos')
 
 
 // interpolando valores template strings (usa a crase `` ) 
-console.log(`o ${name} tem ${age} anos`)
+//console.log(`o ${name} tem ${age} anos`)
 
 
 // Objects 
-
+/*
 const person = {
     name: 'Lilian',
     age: 25,
@@ -40,7 +40,7 @@ const person = {
 console.log(person) 
 
 console.log(person.name) // usar o . (ponto) pega um único valor da propriedade 
-
+*/
 // array 
 
 const animais = [
@@ -53,3 +53,65 @@ const animais = [
 // acessar valores dentro do array
 console.log(animais[0])
 console.log(animais.length) // total de elementos
+
+
+// exercícios 
+// declare uma variável de nome weight 
+var weight
+
+// que tipo de dado é a variável ?
+console.log(typeof weight) // undefined
+
+/* declare uma variável e atribua valores para cada um dos dados: 
+    * name: string
+    * age: number(integer)
+    * stars: number(float)
+    * isSubscribed: boolean 
+
+
+
+
+let name = "Maria"
+let age =  25
+let stars = 26.7
+let isSubscribed = false
+
+*/
+
+
+/* mostre no console a seguinte msg 
+
+    <name> de idade <age> pesa <weight> kg 
+
+*/
+/*
+let student = {
+    name: "Maria",
+    age: 25,
+    stars: 26.7,
+    isSubscribed: false,
+    weight: 70,
+};    
+
+
+console.log (`${student.name} de idade ${student.age} pesa ${student.weight} kg`) */
+
+
+/* Declare uma variável do tipo array, de nome students e atribua a ela nenhum valor, ou seja, somente o array vazio */ 
+
+let student = {
+    name: "Maria",
+    age: 25,
+    stars: 26.7,
+    isSubscribed: false,
+    weight: 70,
+};    
+
+let students = []
+
+// reatribui o valor
+students = [
+    student
+]
+
+console.log(students[0])
