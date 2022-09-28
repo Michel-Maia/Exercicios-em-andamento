2affe4ce7ec90c28106a2a172e6d60cd53a92ad8
Michel Maia <programador.michel.maia@gmail.com>
2022-09-28 15:26:09 -0300

    commit aula JS
    Aula Rocketseat JS - manipulando arrays

--- Javascript/Rocketseat/manipulando_dados/script.js
+++ Javascript/Rocketseat/manipulando_dados/script.js
@@ -67,3 +67,31 @@ console.log(["k", "l", "m", "n"].length)
 
 let word2 = "manipulação"
 console.log(Array.from(word2))
+
+
+// manipulando Arrays 
+
+let techs  = ["html", "css", "js"]
+
+// adicionar um item no fim
+console.log(techs.push("react"))
+
+// adicionar um item no começo
+techs.unshift("sql")
+
+// remover do fim 
+techs.pop()
+
+// remover do começo 
+techs.shift()
+
+// pegar somente alguns elementos do array   (slice significa cortar (1,2 onde começa e termina))
+console.log(techs.slice(1,2))
+
+// remover 1 ou mais itens em qualquer posição do array 
+techs.splice(1,1)
+
+// encontrar a posição de um elemento no array 
+let index = techs.indexOf('html')
+
+console.log(techs)
