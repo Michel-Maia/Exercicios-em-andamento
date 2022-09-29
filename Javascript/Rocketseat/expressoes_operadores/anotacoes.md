39f1cfacc9a2642cdc585afcf63793d4f88cf6dd
Michel Maia <programador.michel.maia@gmail.com>
2022-09-29 11:57:07 -0300

    commit aula Javascript
    Aula Javascript da Rocketseat - operadores

--- Javascript/Rocketseat/expressoes_operadores/anotacoes.md
+++ Javascript/Rocketseat/expressoes_operadores/anotacoes.md
@@ -13,3 +13,19 @@
 - Operadores unários (2 mais comuns)
     typeof
     delete
+
+- Operadores Aritiméticos 
+    Multiplicação
+    Divisão
+    Soma
+    Subtração 
+    Resto da divisão 
+    Incremento
+    Decremento
+    Exponencial
+
+- Grouping operator ( ) 
+    agrupamento feito pelo ()
+
+- Operadores de comparação 
+    Irá comparar valores e retornar um boolean como resposta à comparação 
--- Javascript/Rocketseat/expressoes_operadores/script.js
+++ Javascript/Rocketseat/expressoes_operadores/script.js
@@ -30,3 +30,85 @@ const person = {
 delete person.age
 
 console.log(person)
+
+// Multiplicação
+console.log(3*5)
+
+console.log(5.2*9)
+
+// Divisão
+console.log(12/6)
+
+// Soma
+console.log(5+22)
+
+// Subtração 
+console.log(15-8)
+
+// Resto da divisão % 
+let remainder
+remainder = 11 % 9
+
+console.log(remainder)
+
+// Incremento ++
+let increment = 0
+increment++
+increment++
+increment++
+console.log(increment)
+
+// Decremento --
+let decrement = 0
+decrement--
+decrement--
+console.log(decrement)
+
+// Exponencial **
+console.log(3**2)
+
+// agrupamento ( )
+
+let total = (2 + 3) * 5
+console.log(total)
+
+let total1 = ("Blá blá" + " ti ti ti")
+console.log(total1)
+
+// operadores de comparação 
+
+let one = 1
+let two = 2 
+
+// == igual a 
+console.log(two == 1)
+console.log(one == "1")
+
+// != diferente de
+console.log(one != two)
+console.log(one != 1)
+console.log(one != "1")
+
+// estritamente igual a 
+console.log( one === "1")
+console.log( one === 1)
+
+// estritamente diferente de 
+console.log(two !== "2")
+console.log(two !== 2)
+
+// maior que >
+console.log(one > two)
+
+// maior igual a >=
+console.log(one >= 1)
+console.log(two >= 1)
+
+// menor que <
+console.log(one < two)
+
+// menor igual a <=
+console.log(one <= two)
+console.log(one <= 1)
+console.log(one <= 0)
+
