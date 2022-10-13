06c642195b43fd6d4b42a4aa0322ff286f82e671
Michel Maia <programador.michel.maia@gmail.com>
2022-10-13 18:26:44 -0300

    commit

--- Javascript/Rocketseat/desafios/004script.js
+++ Javascript/Rocketseat/desafios/004script.js
@@ -51,3 +51,60 @@ const booksByCategory = [
         ],
     },
 ];
+
+const totalCategories = booksByCategory.length
+
+console.log(totalCategories);
+for(let category of booksByCategory) {
+    console.log('Total de livros da categoria: ', category.category)
+    console.log(category.books.length)
+}
+
+function countAuthors() {
+    let authors = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(authors.indexOf(book.author) == -1) {
+                authors.push(book.author)
+            }
+        }
+    }
+
+    console.log("Total de autores: ", authors.length)
+}
+
+countAuthors();
+
+
+function booksOfRobert() {
+    let books = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(book.author === 'Robert C. Martin') {
+                books.push(book.title)
+            }
+        }
+    }
+
+    console.log("Livros do autor: ", books)
+}
+
+booksOfRobert();
+
+function booksOfAuthor(author) {
+    let books = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(book.author === author) {
+                books.push(book.title)
+            }
+        }
+    }
+
+    console.log(`Livros do autor ${author}: ${books.join(", ")}`)
+}
+
+booksOfAuthor('Robert C. Martin');
