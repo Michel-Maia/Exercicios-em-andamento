868594c99f188ac793b1599cf0b0c87773759240
Michel Maia <programador.michel.maia@gmail.com>
2022-10-17 18:31:52 -0300

    commit

--- Javascript/Rocketseat/dom/anotacoes.md
+++ Javascript/Rocketseat/dom/anotacoes.md
@@ -11,3 +11,26 @@
 - Manipular o HTML com o Javascript 
 - Você só programa pra WEB porque existe a DOM 
 
+
+## Encapsulamento 
+
+- Colocar numa cápsula
+- Agrupamento de funções e variáveis
+- Esconder detalhes de implementação
+- Camada de segurança para os atributos e métodos
+
+
+## programação estruturada vs programação orientada a objetos
+
+### Programação Estruturada
+- Processa a entrada e manipulação dos dados, até a saída
+- Uso de sequência, estruturas de repetições e condições
+- Uso de uma rotina maior, ou sub-rotinas
+- não existem restrições às variáveis 
+
+### Programação Orientada a Objetos 
+- Surge para trazer um cuidado ao uso estruturado
+- não elimina por completo o uso estruturado 
+- Conceitos como objetos e classes
+- Cuidados com variáveis e rotinas (Encapsulamento)
+- Melhor reuso de código (Herança)
--- Javascript/Rocketseat/dom/script.js
+++ Javascript/Rocketseat/dom/script.js
@@ -68,3 +68,74 @@ const element9 = document.querySelector('.two')
 
 element9.style.backgroundColor = '#ff4500'
 
+
+// Estrutural 
+let altura1 = 50
+let largura1 = 60
+
+function calcularArea() {
+    return altura1 * largura1 
+}
+
+let area = calcularArea()
+
+// Orientado a Objetos
+class Poligono {
+    constructor(altura, largura) {
+        this.altura = altura 
+        this.largura = largura
+    }
+   
+    get area() {
+    return this.#calcularArea()
+    }
+
+    #calcularArea() {
+        return this.altura * this.largura
+    }
+
+}
+
+let poligono = new Poligono(50, 60)
+console.log(poligono.area)
+
+
+
+// estruturado 
+var valorHora = 50
+var tempoEstimado = 20
+var desconto = valorHora * tempoEstimado * (10/100)
+var custoEstimado = valorHora * tempoEstimado - desconto
+console.log(custoEstimado)
+
+// POO
+const job = new Job()
+job.valorHora = 50
+job.tempoEstimado = 20
+job.desconto = 10
+const custoEstimado = job.calcularEstimativa()
+console.log(custoEstimado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
