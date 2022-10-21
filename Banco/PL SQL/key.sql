d841c4cf638ebd338fd31993064bfb3898ba6cd8
Michel Maia <programador.michel.maia@gmail.com>
2022-10-21 14:53:57 -0300

    commit

--- Banco/PL SQL/key.sql
+++ Banco/PL SQL/key.sql
@@ -1,3 +1,52 @@
 SELECT * FROM meus_pais;
 
--- https://www.youtube.com/watch?v=flHpm__7h2s&list=PLYp15S-UDtuNxSlRnFTdCjCl3LLAawbSH&index=6
+SELECT * FROM filhos;
+
+ALTER TABLE meus_pais ADD PRIMARY KEY (codigo);
+
+CREATE TABLE filhos
+(
+    cod_pai NUMBER(5),
+    cod_filho NUMBER(5),
+    nome VARCHAR2(50),
+    idade NUMBER,
+    sexo CHAR(1),
+    FOREIGN KEY (cod_pai)
+    REFERENCES meus_pais (codigo)
+);
+
+INSERT INTO filhos
+(
+    cod_pai, cod_filho, nome, idade, sexo
+) VALUES (
+    2,
+    1,
+    'filho 1->1',
+    10,
+    'M'
+);
+
+-- select com duas ou mais tabelas usar JOIN
+
+SELECT pai.nome, filhos.nome, filhos.idade, filhos.sexo
+FROM meus_pais pai,
+    filhos filhos
+WHERE filhos.cod_pai = pai.codigo;
+
+SELECT * FROM filhos;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
