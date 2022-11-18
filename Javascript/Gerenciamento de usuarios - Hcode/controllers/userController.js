09e9eee78128a42eb365b5af23c0924bbf9a157e
Michel Maia <programador.michel.maia@gmail.com>
2022-11-18 14:54:25 -0300

    commit

--- Javascript/Gerenciamento de usuarios - Hcode/controllers/userController.js
+++ Javascript/Gerenciamento de usuarios - Hcode/controllers/userController.js
@@ -13,10 +13,41 @@ class UserController {
         this.formEl.addEventListener("submit", event => {
 
             event.preventDefault();
-        
-            this.addLine(this.getValues());
+
+            let values = this.getValues();
+
+            
+
+            this.getPhoto((content) =>{
+            
+                values.photo = content;
+                
+                this.addLine(values);
+            }); 
         });
     }
+
+    getPhoto(callback){
+
+        let fileReader = new FileReader();
+
+       let elements = [...this.formEl.elements].filter(item=> {
+          if  (item.name === 'photo') {
+                   return item;
+            }
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+
+          
+            callback(fileReader.result);
+        };
+
+        fileReader.readAsDataURL(file);
+    }
+
 
     getValues(){
 
@@ -54,7 +85,7 @@ class UserController {
 
         this.tableEl.innerHTML=`
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
