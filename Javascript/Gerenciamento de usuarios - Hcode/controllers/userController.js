abc76960997ec33e5fb0b9596d94ddb12509927b
Michel Maia <programador.michel.maia@gmail.com>
2022-11-18 13:34:58 -0300

    commit

--- Javascript/Gerenciamento de usuarios - Hcode/controllers/userController.js
+++ Javascript/Gerenciamento de usuarios - Hcode/controllers/userController.js
@@ -22,7 +22,7 @@ class UserController {
 
         let user = {};
 
-        this.formEl.elements.forEach(function (field, index) {
+        [...this.formEl.elements].forEach(function (field, index) {
 
             if (field.name == "gender") {
                 if (field.checked) 
