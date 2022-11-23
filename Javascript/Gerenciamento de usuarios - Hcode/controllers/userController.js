88c4570ec11aa36f961a334c153173341f5a9aaa
Michel Maia <programador.michel.maia@gmail.com>
2022-11-23 13:21:13 -0300

    commit

--- Javascript/Gerenciamento de usuarios - Hcode/controllers/userController.js
+++ Javascript/Gerenciamento de usuarios - Hcode/controllers/userController.js
@@ -14,6 +14,10 @@ class UserController {
 
             event.preventDefault();
 
+           let btn = this.formEl.querySelector("[type=submit]");
+            
+            btn.disabled = true;
+
             let values = this.getValues();
 
             this.getPhoto().then(function(content){
@@ -27,6 +31,10 @@ class UserController {
                 values.photo = content;
                 
                 this.addLine(values);
+
+                this.formEl.reset();
+
+                btn.disabled = false;
             },
             (e) => {
                 console.error(e);
@@ -122,7 +130,7 @@ class UserController {
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${(dataUser.admin)? 'Sim' : 'Não'}</td>
-                <td>${dataUser.birth}</td>
+                <td>${dataUser.register}</td>
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
