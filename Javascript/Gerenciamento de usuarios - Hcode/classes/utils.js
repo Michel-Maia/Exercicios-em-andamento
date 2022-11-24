8fd9acd3b354fe4d1109ac61d85c480890966607
Michel Maia <programador.michel.maia@gmail.com>
2022-11-24 15:43:04 -0300

    commit

---
+++ Javascript/Gerenciamento de usuarios - Hcode/classes/utils.js
@@ -0,0 +1,8 @@
+class utils {
+    
+
+    static dateFormat(date) {
+
+        return date.getDate()+'/'+(date.getMonth()+1)+ '/' +date.getFullYear()+''+date.getHours()+':'+date.getMinutse();
+    }
+}
