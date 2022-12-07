dbf7aebf27341e5819272f1b1d27d73f6278874c
Michel Maia <programador.michel.maia@gmail.com>
2022-12-07 18:28:11 -0300

    commit

---
+++ Javascript/Alura/canvas/script01.js
@@ -0,0 +1,36 @@
+var tela = document.querySelector('canvas');
+var pincel = tela.getContext('2d');
+
+
+pincel.fillStyle = 'lightgrey';
+pincel.fillRect(0, 0, 600, 400);
+
+
+// fillReact preencha o retângulo
+// fill preencha  
+
+pincel.fillStyle = 'green';
+pincel.fillRect(0, 0, 200, 400);
+
+pincel.fillStyle = 'red';
+pincel.fillRect(400, 0, 200, 400);
+
+// começar novo caminho 
+// begin path
+
+pincel.fillStyle = 'yellow';
+pincel.beginPath();
+pincel.moveTo(300, 200);
+pincel.lineTo(200, 400);
+pincel.lineTo(400, 400);
+pincel.fill();
+
+
+pincel.fillStyle = 'blue';
+pincel.beginPath();
+pincel.arc(300, 200, 50, 0, 2 * 3.14);
+pincel.fill();
+
+
+
+
