8558927c28ca0da73c702f82af6302d88820d4de
Michel Maia <programador.michel.maia@gmail.com>
2022-12-21 21:39:32 -0300

    commit

---
+++ Javascript/Alura/02exercicios/007script.js
@@ -0,0 +1,23 @@
+// ex de erros
+
+//const numero; 
+// erro Missing initializer in const declaration
+// Inicializador ausente na declaração const
+
+//const numero = 1;
+//console.log(numero;
+// missing ) after argument list
+// faltando ) depois da lista de argumentos
+
+//console.log(minhaVar);
+// minhaVar is not defined
+// minhaVar não está definida
+
+
+
+
+
+
+
+
+
