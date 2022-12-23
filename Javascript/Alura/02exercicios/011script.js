c1eaacd3b6a0af705d036f325bec6e8a1f0e86d1
Michel Maia <programador.michel.maia@gmail.com>
2022-12-23 11:27:11 -0300

    commit

---
+++ Javascript/Alura/02exercicios/011script.js
@@ -0,0 +1,20 @@
+// operador ternário
+
+const idadeMinima = 18;
+//const idadeCliente = 16;
+const idadeCliente = 30;
+
+//if (idadeCliente >= idadeMinima){
+//    console.log("bebida")
+//} else {
+//    console.log("refri")
+//}
+
+
+               // condição              ?  true     : false  
+console.log(idadeCliente >= idadeMinima ? "bebida" : "refri");
+
+
+
+
+
