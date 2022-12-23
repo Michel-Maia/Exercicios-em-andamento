07ed40238214ae88eaf2ad1528d10812f202a227
Michel Maia <programador.michel.maia@gmail.com>
2022-12-23 10:57:38 -0300

    commit

---
+++ Javascript/Alura/02exercicios/008script.js
@@ -0,0 +1,24 @@
+// log -> registro
+// https://nodejs.org/api/console.html
+
+
+const minhaVar = true;
+
+console.log(245);
+console.log("eu sou um texto");
+console.log(minhaVar);
+
+
+// tratamento de erro
+
+console.error("deu erro");
+
+/*
+console.error() para exibir mensagens de erro;
+console.table() para visualizar de forma mais organizada informações tabulares;
+console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
+console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
+*/
+
+
+
---
+++ Javascript/Alura/02exercicios/009script.js
@@ -0,0 +1,23 @@
+console.log("deu erro");
+console.error(new Error("deu erro"));
+
+
+/*
+deu erro
+Error: deu erro
+    at Object.<anonymous> (D:\.009script.js:2:15)    
+    at Module._compile (node:internal/modules/cjs/loader:1101:14)
+    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1153:10)      
+    at Module.load (node:internal/modules/cjs/loader:981:32)
+    at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81
+:12)
+    at node:internal/main/run_main_module:17:47
+*/
+
+
+
+
+
+
+
