e65a2d54b2246f9ce401461f6999023fc1048b86
Michel Maia <programador.michel.maia@gmail.com>
2022-12-23 11:15:09 -0300

    commit

---
+++ Javascript/Alura/02exercicios/010script.js
@@ -0,0 +1,26 @@
+// operadored de comparação 
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators
+
+
+const numero = 7;
+const texto = "7";
+
+console.log(numero == texto);
+
+console.log(numero === texto);
+
+// typeof
+console.log(typeof numero);
+console.log(typeof texto);
+
+/*
+ == compara o valor
+ === compara o valor e o tipo de dado
+*/
+
+// conversão explícita
+Number()
+String()
+
+
+
