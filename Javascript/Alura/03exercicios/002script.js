068f522fed419754c3d240d00aeee33f4e797efb
Michel Maia <programador.michel.maia@gmail.com>
2022-12-30 18:56:32 -0300

    commit

---
+++ Javascript/Alura/03exercicios/002script.js
@@ -0,0 +1,12 @@
+console.log("Atribuição de variáveis");
+
+const idade = 50;
+const nome = "Enzo";
+const sobrenome = "Ferrari";
+
+console.log(nome + sobrenome);
+console.log(nome, sobrenome);
+console.log(nome + " " + sobrenome);
+
+
+
