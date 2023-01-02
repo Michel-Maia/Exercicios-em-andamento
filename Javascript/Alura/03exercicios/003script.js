cb0de78fa8fb40af45e7fb60197b95602ad372f0
Michel Maia <programador.michel.maia@gmail.com>
2023-01-02 07:39:02 -0300

    commit

---
+++ Javascript/Alura/03exercicios/003script.js
@@ -0,0 +1,30 @@
+//let contador = 0;
+//contador = contador +1;
+
+console.log(`Listas`);
+
+const salvador = `Salvador`;
+const saoPaulo = `São Paulo`;
+const rioDeJaneiro = `Rio de Janeiro`;
+
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`) // add um item na lista 
+console.log(listaDeDestinos);
+
+
+listaDeDestinos.splice(2,1) // deleta um item na lista  (posição , qnt)
+console.log(listaDeDestinos);
+
+
+
+
+
+
+
+
