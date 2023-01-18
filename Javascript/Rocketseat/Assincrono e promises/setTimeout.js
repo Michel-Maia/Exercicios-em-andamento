e2dfcb98243a11e6bce62d947a7d41c3119a733d
Michel Maia <programador.michel.maia@gmail.com>
2023-01-18 18:49:51 -0300

    commit

---
+++ Javascript/Rocketseat/Assincrono e promises/setTimeout.js
@@ -0,0 +1,9 @@
+// setTimeout(function, delay)
+// função e tempo que será executada
+
+
+setTimeout(function() {
+    console.log('depois de 1s')
+}, 1000)
+
+
---
+++ Javascript/Rocketseat/functions/17teste.js
@@ -0,0 +1,22 @@
+// teste 
+function arraySimples(ar) {
+    let soma = 0;
+    for (let index = 0; index < ar.lenght; index += 1){
+        soma = soma + ar[index];
+    }
+    return soma; 
+};
+
+arraySimples([1,2,3]);
+
+
+
+
+
+
+
+
+
+
+
+
