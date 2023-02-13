ee0dc7a59ac31e4bb25a8fc2010dad0d2b8559ed
Michel Maia <programador.michel.maia@gmail.com>
2023-02-13 19:16:57 -0300

    commit

---
+++ ReactJS/treinamento/treinamento-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import HelloWorld from './components/helloWorld';
+
+function App() {
+  return (
+   <div className='App'>
+    <HelloWorld/>
+   </div>
+   
+  );
+}
+
+export default App;
